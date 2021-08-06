1cf0d40c9646c38ecb92814c98ae9bf44e6fc802
Saurav Kumar <67797045+rjsaurav13@users.noreply.github.com>
2021-08-06 21:05:42 +0530

    Update Wordcount.cpp

--- Wordcount.cpp
+++ Wordcount.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         string str; 
-        if(s==" "){
-            return 0;
-        }
+        
         stringstream str_strm(s);
         vector<string> sen; 
         while (str_strm >> str) {
